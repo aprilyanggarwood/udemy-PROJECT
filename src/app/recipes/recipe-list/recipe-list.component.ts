39146d943e82c0152a8f1e415002f4a2d349a350
aprilyanggarwood <yangyyj822@hotmail.com>
2023-09-28 02:38:26 -0700

    continue learning

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2023/07/27/07/42/cherry-cake-8152717_1280.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2023/07/27/07/42/cherry-cake-8152717_1280.jpg')
+
+  ];
 
   constructor() { }
 
